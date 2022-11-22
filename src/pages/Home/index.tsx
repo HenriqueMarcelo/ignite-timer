46c06bc8b17fbe46cdf29a7f94ae06e12acb8ce4
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2022-11-22 15:33:10 -0300

    Improving Form Inputs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -19,13 +19,23 @@ export function Home() {
             placeholder="Dê um nome para o seu projeto"
             type="text"
             id="task"
+            list="task-suggestions"
           />
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Banana" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             placeholder="00"
             type="number"
             id="minutesAmount"
+            step={5}
+            min={5}
+            max={60}
           />
 
           <span>minutos.</span>
