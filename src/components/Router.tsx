05434018b434bf43c15ca9ab1bf55132eba0af05
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2022-11-22 14:43:34 -0300

    Header Component

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
-import { DefaultLayout } from '../layouts/DefautLayout'
+import { DefaultLayout } from '../layouts/DafultLayout'
 import { History } from '../pages/History'
 import { Home } from '../pages/Home'
 
