d69f8aea55c1b407c6b1e853a778b20b3ac854d9
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2022-11-23 11:18:48 -0300

    TypeScript on the Form

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -21,12 +21,19 @@ const newCycleFormValidationSchema = zod.object({
     .max(60, 'O ciclo precisa ser de no máximo 60 minutos.'),
 })
 
+// A biblioteca Zod cria um tipo atrável de uma Schema
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
 export function Home() {
-  const { register, handleSubmit, watch } = useForm({
+  const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
   })
 
-  function handleCreateNewCycle(data: any) {
+  function handleCreateNewCycle(data: NewCycleFormData) {
     console.log(data)
   }
 
