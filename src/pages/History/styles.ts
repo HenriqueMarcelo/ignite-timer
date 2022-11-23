93ac3b922135ade52234f157f83c864be6531270
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2022-11-23 09:49:40 -0300

    Status component

--- src/pages/History/styles.ts
+++ src/pages/History/styles.ts
@@ -59,3 +59,28 @@ export const HistoryList = styled.div`
     }
   }
 `
+
+const STATUS_COLORS = {
+  yellow: 'yellow-500',
+  green: 'green-500',
+  red: 'red-500',
+} as const // Para falar que são só essas opções e nunca vai mudar
+
+interface StatusProps {
+  statusColor: keyof typeof STATUS_COLORS // Para informar que as opções são as chaves
+}
+
+export const Status = styled.span<StatusProps>`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  &::before {
+    content: '';
+    width: 0.5rem;
+    height: 0.5rem;
+    border-radius: 999px;
+    background-color: ${({ theme, statusColor }) =>
+      theme[STATUS_COLORS[statusColor]]};
+  }
+`
